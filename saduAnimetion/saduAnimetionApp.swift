9f1b8a7678644eef61f4657362de80d575cb6f9e
malak kariri <46464799+malakkariri@users.noreply.github.com>
2023-10-11 11:19:45 +0300

    Initial Commit

---
+++ saduAnimetion/saduAnimetionApp.swift
@@ -0,0 +1,17 @@
+//
+//  saduAnimetionApp.swift
+//  saduAnimetion
+//
+//  Created by malak kariri on 26/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct saduAnimetionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
